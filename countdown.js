f0259c32128d8bd864ce4f2ca97315781be2c350
freya0608 <freya001@yeah.net>
2017-04-01 23:52:48 +0800

    countdown

--- countdown.js
+++ countdown.js
@@ -1,3 +1,45 @@
 /**
  * Created by freya on 2017/4/1.
  */
+
+var WINDOW_WIDTH =1024;
+var WINDOW_HEIGHT =768;
+
+window.onload = function () {
+    var canvas = document.getElementsByClassName('canvas');
+    var context = document.getContext('2d');
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    render(context);
+
+};
+
+function render(cxt) {
+    var hours = 12;
+    var minutes = 34;
+    var seconds = 56;
+
+    renderDigit(0,0,parseInt(hours/10,cxt));
+}
+
+function renderDigit(x,y,num,cxt) {
+    cxt.fillStyle = 'rgb(0,102,153)';
+
+    for(var i = 0;i<digit[num].length;i++){
+        for(var j= 0;j<digit[num];j++){
+            if(digit[num][i][j] == 1){
+                cxt.beginPath();
+                cxt.acc();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
