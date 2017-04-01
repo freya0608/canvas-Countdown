563bc6ffed39876f0b512e513e9458cfdf83c565
freya0608 <freya001@yeah.net>
2017-04-02 00:13:58 +0800

    init

--- countdown.js
+++ countdown.js
@@ -4,10 +4,13 @@
 
 var WINDOW_WIDTH =1024;
 var WINDOW_HEIGHT =768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
 
 window.onload = function () {
-    var canvas = document.getElementsByClassName('canvas');
-    var context = document.getContext('2d');
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
 
     canvas.width = WINDOW_WIDTH;
     canvas.height = WINDOW_HEIGHT;
@@ -21,17 +24,22 @@ function render(cxt) {
     var minutes = 34;
     var seconds = 56;
 
-    renderDigit(0,0,parseInt(hours/10,cxt));
+    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
+    renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
+    renderDigit(MARGIN_LEFT+30*(RADIUS+1),MARGIN_TOP,10,cxt);
 }
 
 function renderDigit(x,y,num,cxt) {
-    cxt.fillStyle = 'rgb(0,102,153)';
-
+    cxt.fillStyle = "rgb(0,102,153)";
+    
     for(var i = 0;i<digit[num].length;i++){
-        for(var j= 0;j<digit[num];j++){
+        for(var j= 0;j<digit[num][i].length;j++){
             if(digit[num][i][j] == 1){
                 cxt.beginPath();
-                cxt.acc();
+                cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1), RADIUS, 0, 2*Math.PI);
+                cxt.closePath();
+
+                cxt.fill();
             }
         }
     }
