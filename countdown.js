1b47e921f19afeadf1a18e35fcd11ef7b9f1f304
freya0608 <freya001@yeah.net>
2017-04-02 12:44:50 +0800

    time

--- countdown.js
+++ countdown.js
@@ -8,7 +8,7 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-const endTime = new Date(2017,3,5,18,47,52);
+const endTime = new Date(2017,3,2,18,47,52);//设置时间需要不超过100小时
 
 var curShowTimeSeconds = 0;
 
@@ -20,36 +20,74 @@ window.onload = function () {
     canvas.height = WINDOW_HEIGHT;
     curShowTimeSeconds = getCurrentShowTimeSeconds();
 
-    render(context);
+
+    setInterval(
+        function () {
+        render(context);
+        update();
+    },50)
+
+
 
 };
+
 
 function getCurrentShowTimeSeconds() {
     var curTime = new Date();
     var ret = endTime.getTime() - curTime.getTime();
-    ret = Math.round(ret/100);
+    //console.log(ret);
+    ret = Math.round(ret/1000);
 
     return ret>=0?ret:0;
 }
 
+
+
+function update() {
+
+    var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    var nextHours = parseInt(nextShowTimeSeconds/3600);
+    var nextMinutes = parseInt((nextShowTimeSeconds-nextHours*3600)/60);
+    var nextSeconds = nextShowTimeSeconds%60;
+
+
+    var curHours = parseInt(curShowTimeSeconds/3600);
+    var curMinutes = parseInt((curShowTimeSeconds-curHours*3600)/60);
+    var curSeconds = curShowTimeSeconds%60;
+
+
+    if(nextSeconds != curSeconds){
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+}
+
 function render(cxt) {
+
+    cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+
     var hours = parseInt(curShowTimeSeconds/3600);
+
+    console.log(parseInt(hours/10));
+    console.log(parseInt(hours));
     var minutes = parseInt((curShowTimeSeconds-hours*3600)/60);
+    console.log(minutes);
     var seconds = curShowTimeSeconds%60;
+   console.log(seconds);
 
     renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
     renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
     renderDigit(MARGIN_LEFT+30*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
+    renderDigit(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10),cxt);
     renderDigit(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
     renderDigit(MARGIN_LEFT+69*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
+    renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),cxt);
     renderDigit(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
 }
 
 function renderDigit(x,y,num,cxt) {
     cxt.fillStyle = "rgb(0,102,153)";
-    console.log(digit);
+    //console.log(digit);
 
     for(var i = 0;i<digit[num].length;i++){
         for(var j= 0;j<digit[num][i].length;j++){
@@ -57,7 +95,6 @@ function renderDigit(x,y,num,cxt) {
                 cxt.beginPath();
                 cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1), RADIUS, 0, 2*Math.PI);
                 cxt.closePath();
-
                 cxt.fill();
             }
         }
